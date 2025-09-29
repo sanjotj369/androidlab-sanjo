1ccf4daffc72a9f3fd173306bbf0c6f960cb0b7d
sanjotj369 <sanjothadathil@gmail.com>
2025-09-29 15:17:05 +0530

    MainActrivty2.java

---
+++ INTENT/MainActrivty2.java
@@ -0,0 +1,40 @@
+package com.example.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Bundle bundle = getIntent().getExtras();
+        if (bundle != null) {
+            String imgindex = bundle.getString("ImageIndex");
+            setImage(imgindex);
+        }
+    }
+
+    private void setImage(String index) {
+        ImageView imageView = (ImageView) findViewById(R.id.img1);
+
+        switch (index) {
+            case "1":
+                imageView.setImageResource(R.drawable.daisy);
+                break;
+            case "2":
+                imageView.setImageResource(R.drawable.images);
+                break;
+            case "3":
+                imageView.setImageResource(R.drawable.tulips);
+                break;
+            default:
+                Toast.makeText(this, "Index is not available", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
